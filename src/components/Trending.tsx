3d1d0d2c54b7f94059e1498e531a810eb24802e6
Christopher Ang <chrisbryanang@gmail.com>
2024-01-20 17:59:56 -0800

    Added title card for Trending movies and bookmark button.

--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -6,9 +6,35 @@ const Trending = () => {
 			<p>Trending</p>
 			<div className="flex w-max space-x-4">
 				{Trends?.map((trend, idx: number) => (
-					<div className="block h-[150px] w-[280px]">
+					<div className="relative h-[150px] w-[280px]">
+						<div className="absolute flex flex-col p-2 justify-between h-full w-full">
+							<div className="flex justify-center items-center self-end bg-black/40 rounded-full h-8 w-8">
+								<img alt="bookmark" src="assets/icon-bookmark-empty.svg" />
+							</div>
+							<div className="pl-2 pb-1">
+								<div className="flex items-center space-x-2 mb-1 text-gray-300 text-sm">
+									<p>{trend.year}</p>
+									<p>•</p>
+									<div className="flex justify-center items-center gap-2">
+										<img
+											alt={trend.category}
+											src={
+												trend.category === "Movie"
+													? "./assets/icon-category-movie.svg"
+													: "./assets/icon-category-tv.svg"
+											}
+											className="h-4 w-4"
+										/>
+										<p>{trend.category}</p>
+									</div>
+									<p>•</p>
+									<p>{trend.rating}</p>
+								</div>
+								<p className="text-lg font-normal">{trend.title}</p>
+							</div>
+						</div>
 						<img
-							className="h-full w-full rounded-lg"
+							className="block h-full w-full rounded-lg"
 							alt={`trend ${idx}`}
 							src={trend.thumbnail.trending?.small}
 						/>
